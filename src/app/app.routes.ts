47ae0f05535c84bed15d88e72c2ebcdcf4424bd6
Andres Astra <andieroid@arizona.edu>
2024-02-28 13:51:46 -0500

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,14 @@ export const routes: Routes = [
   {
     path: 'events',
     loadComponent: () => import('./features/events/events.component').then((c) => c.EventsComponent)
-  }
+  },
+
+  {
+      path: 'login',
+      loadComponent: () => import('./features/auth/login/login.component').then((c) => c.LoginComponent)
+  },
+
+
+
+
 ];
